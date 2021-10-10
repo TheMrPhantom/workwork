6e2b91e3fd55b2ae78e279208e4adde9bc96ad98
schieljn <diemilbe@gmx.net>
2021-10-10 07:08:26 +0200

    more functions

--- frontend/src/Components/SportManagement/AddSportEntry.js
+++ frontend/src/Components/SportManagement/AddSportEntry.js
@@ -1,9 +1,21 @@
+import { Button, TextField } from '@material-ui/core'
 import React from 'react'
+import Spacer from '../Common/Spacer'
+import AddBoxIcon from '@material-ui/icons/AddBox';
 
 const AddSportEntry = () => {
     return (
-        <div>
-            
+        <div className="outterAddBoxSport">
+            <div className="innerBoxSport">
+            <TextField label="Name" type="input"  />
+                <Spacer horizontal={30}></Spacer>
+                <TextField label="Extra Arbeitsstunden" type="number" defaultValue={0} />
+            </div>
+            <div>
+                <Button >
+                    <AddBoxIcon className="successBackground" />
+                </Button>
+            </div>
         </div>
     )
 }
--- frontend/src/Components/SportManagement/SportManagement.js
+++ frontend/src/Components/SportManagement/SportManagement.js
@@ -3,18 +3,25 @@ import React from 'react'
 import Spacer from '../Common/Spacer'
 import SportEntry from './SportEntry'
 import SaveIcon from '@material-ui/icons/Save';
+import AddSportEntry from './AddSportEntry';
 
 const SportManagement = () => {
     return (
         <div>
             <Typography variant="h5">Sportarten</Typography>
             <Spacer vertical={20}/>
+            <Spacer vertical={5}/>
             <SportEntry name="Agility" extraHours={2}/>
             <Spacer vertical={5}/>
             <SportEntry name="TurnierHunde" extraHours={1}/>
             <Spacer vertical={5}/>
             <SportEntry name="Rettungshunde" extraHours={0}/>
-
+            <Spacer vertical={50}/>
+            
+            <Typography variant="h5">Sportart Hinzufügen</Typography>
+            <Spacer vertical={5}/>
+            <AddSportEntry />
+            
             <Fab size="medium" variant="extended" className="fixedButton accept">
                 <SaveIcon sx={{ mr: 1 }} />
                 Speichern
