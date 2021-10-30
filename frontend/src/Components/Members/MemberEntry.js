df0c294e35639fd1e3084a9a52a3db959aaa5a95
schieljn <diemilbe@gmx.net>
2021-10-30 14:29:29 +0200

    fixed #19

--- frontend/src/Components/Members/MemberEntry.js
+++ frontend/src/Components/Members/MemberEntry.js
@@ -1,28 +1,48 @@
 import { Button, LinearProgress, Paper, Typography } from '@material-ui/core'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Spacer from '../Common/Spacer'
 import EditIcon from '@material-ui/icons/Edit';
 import { useHistory } from "react-router-dom";
 import "./MemberEntry.css"
 
-const MemberEntry = ({ name, currentWork, maxWork, hasWorkHours ,id}) => {
-    
-    const history=useHistory();
-    
-    const calcProgress = () => {
-        return (currentWork / maxWork) * 100
+const MemberEntry = ({ name, currentWork, maxWork, hasWorkHours, id, sportNames }) => {
+
+    const [progressEntries, setprogressEntries] = useState([])
+
+    useEffect(() => {
+        if (!hasWorkHours) {
+            setprogressEntries([])
+        }
+        
+        var progressEntryTemp = []
+        for (var i = 0; i < Math.min(currentWork.length, maxWork.length, name.length); i++) {
+            progressEntryTemp.push({ name: sportNames[i], currentWork: currentWork[i].hours, maxWork: maxWork[i].hours })
+        }
+
+        console.log(progressEntryTemp)
+        setprogressEntries(progressEntryTemp)
+    }, [currentWork, maxWork, sportNames])
+
+    const history = useHistory();
+    const calcProgress = (cW, mW) => {
+        return (cW / mW) * 100
     }
+
     return (
         <Paper elevation={2} className="outterBoxMember">
             <div className="innerFlexMember">
                 <Typography>{name}</Typography>
-                {hasWorkHours?<Spacer horizontal={20} />:""}
-                {hasWorkHours?<LinearProgress variant="determinate" value={calcProgress()} style={{ width: "50%" }} />:""}
-                {hasWorkHours?<Spacer horizontal={20} />:""}
-                {hasWorkHours?<Typography>{currentWork}/{maxWork}</Typography>:""}
+                {hasWorkHours ? <Spacer horizontal={20} /> : ""}
+                {progressEntries.map((value) => <div style={{minWidth:"100px",marginRight:"10px"}}>
+                    <Typography>{value.name}</Typography>
+                    <Spacer horizontal={10} />
+                    <LinearProgress variant="determinate" value={calcProgress(value.currentWork, value.maxWork)} style={{ width: "100%" }} />
+                    <Spacer horizontal={20} />
+                    <Typography>{value.currentWork}/{value.maxWork}</Typography>
+                </div>)}
             </div>
             <div>
-                <Button onClick={()=>history.push('/members/edit/'+id)}>
+                <Button onClick={() => history.push('/members/edit/' + id)}>
                     <EditIcon />
                 </Button>
             </div>
--- frontend/src/Components/Members/Members.js
+++ frontend/src/Components/Members/Members.js
@@ -7,14 +7,29 @@ import AddMember from './AddMember'
 import "./MemberEntry.css"
 const Members = () => {
     const [members, setmembers] = useState([])
+    const [filteredState, setfilteredMembers] = useState([])
     const [displayedMembers, setdisplayedMembers] = useState([])
     const [refresh, setrefresh] = useState(false)
 
     useEffect(() => {
         getAndStore("members", setmembers)
-        getAndStore("members", setdisplayedMembers)
+        getAndStore("members", setfilteredMembers)
 
     }, [refresh])
+
+    useEffect(() => {
+        const newArray = []
+        filteredState.forEach((value) => {
+            const sportNames = []
+            const works = []
+            value.currentWork.forEach((value) => { sportNames.push(value.name) })
+            for (var i = 0; i < Math.min(value.currentWork.length, value.maxWork.length); i++) {
+                works.push([value.currentWork[i].hours, value.maxWork[i].hours])
+            }
+            newArray.push({ ...value, sportNames: sportNames, work: works })
+        })
+        setdisplayedMembers(newArray)
+    }, [filteredState])
 
     const filterMembers = (text) => {
         const newList = members.filter((item) => {
@@ -29,10 +44,10 @@ const Members = () => {
 
             return firstname.includes(searchText) || lastname.includes(searchText) || email.includes(searchText)
         })
-        setdisplayedMembers(newList)
+        setfilteredMembers(newList)
     };
 
-    const refreshComponent=()=>{
+    const refreshComponent = () => {
         setrefresh(!refresh)
     }
 
@@ -41,12 +56,12 @@ const Members = () => {
             <Typography variant="h5">Mitglieder</Typography>
             <Spacer vertical={10} />
             <div className="horizontalMemberFloat">
-            <TextField className="reasonBox" label="Suche" type="input" onChange={(value) => filterMembers(value.target.value)} />
-            <AddMember buttonText="Hinzufügen" headlineText="Mitglied hinzufügen" confirmText="Hinzufügen" refresh={refreshComponent}/>
+                <TextField className="reasonBox" label="Suche" type="input" onChange={(value) => filterMembers(value.target.value)} />
+                <AddMember buttonText="Hinzufügen" headlineText="Mitglied hinzufügen" confirmText="Hinzufügen" refresh={refreshComponent} />
             </div>
             <Spacer vertical={20} />
             {displayedMembers.map((value) => {
-                return <div key={value.id}><MemberEntry name={value.firstname + " " + value.lastname} currentWork={value.currentWork} maxWork={value.maxWork} hasWorkHours={!(value.isTrainer||value.isExecutive)} id={value.id}/> <Spacer vertical={2} /></div>
+                return <div key={value.id}><MemberEntry name={value.firstname + " " + value.lastname} sportNames={value.sportNames} currentWork={value.currentWork} maxWork={value.maxWork} hasWorkHours={!(value.isTrainer || value.isExecutive)} id={value.id} /> <Spacer vertical={2} /></div>
             })}
         </div>
     )
