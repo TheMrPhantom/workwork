a53d1ccc15a1b8202413e2c889f30dd7468a3c1c
schieljn <diemilbe@gmx.net>
2022-01-20 23:09:00 +0100

    multiple states for disabling download button and fixed #40

--- frontend/src/Components/Downloads/DownloadOverview.js
+++ frontend/src/Components/Downloads/DownloadOverview.js
@@ -6,24 +6,24 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Spacer from '../Common/Spacer'
 
 const DownloadOverview = () => {
-    const [downloading, setdownloading] = useState(false)
+    const downloading = [{ ...useState(false) }, { ...useState(false) }]
     const buttonStyle = { width: "170px" }
-    const downloadP = async (path) => {
-        setdownloading(true)
+    const downloadP = async (path, id) => {
+        downloading[id][1](true)
         const resp = await downloadPDF(path)
         if (resp === 400) {
             //Something went wrong
         }
-        setdownloading(false)
+        downloading[id][1](false)
     }
 
-    const displayPDFButton = (url) => {
-        if (downloading) {
+    const displayPDFButton = (url, index) => {
+        if (downloading[index][0]) {
             return <Button variant="outlined" disabled style={buttonStyle}>
                 <CircularProgress style={{ color: "var(--primaryColor)", width: "30px", height: "30px" }} />
             </Button>
         } else {
-            return (<Button variant="outlined" onClick={() => downloadP(url)} style={buttonStyle}>
+            return (<Button variant="outlined" onClick={() => downloadP(url, index)} style={buttonStyle}>
                 Download (PDF)
             </Button>
             )
@@ -35,7 +35,7 @@ const DownloadOverview = () => {
             <Typography variant="h6">Mitgliederübersicht</Typography>
             <Spacer vertical={15} />
             <div style={{ display: "flex" }}>
-                {displayPDFButton("report/members/pdf")}
+                {displayPDFButton("report/members/pdf", 0)}
                 <Spacer horizontal={20} />
                 <Button disabled variant="outlined" onClick={() => alert("Noch keine Funktion")}>
                     Download (Excel)
@@ -45,7 +45,7 @@ const DownloadOverview = () => {
             <Typography variant="h6">Mitglieder mit offenen Arbeitsstunden</Typography>
             <Spacer vertical={15} />
             <div style={{ display: "flex" }}>
-                {displayPDFButton("report/remainingWorkhours/pdf")}
+                {displayPDFButton("report/remainingWorkhours/pdf", 1)}
                 <Spacer horizontal={20} />
                 <Button disabled variant="outlined" onClick={() => alert("Noch keine Funktion")}>
                     Download (Excel)
