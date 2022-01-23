420f01e76308f06f84e3568054b6d566caa7ee39
schieljn <diemilbe@gmx.net>
2022-01-23 22:27:26 +0100

    Added sqlalchemy models

---
+++ backend/database/Member.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import sqlalchemy as sql
+from test import db
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Member(db.Model):
+    id = sql.Column(sql.Integer, primary_key=True)
+    firstname = sql.Column(sql.String(100), nullable=False)
+    lastname = sql.Column(sql.String(100), nullable=False)
+    mail = sql.Column(sql.String(100), nullable=False)
+    role = sql.Column(sql.Integer, nullable=False)
+    deleted = sql.Column(sql.Boolean, nullable=False)
+    salt = sql.Column(sql.String(32), nullable=False)
+    extra_hours = sql.Column(sql.Integer, nullable=False)
+    last_modified = sql.Column(sql.DateTime, default=datetime.utcnow)
---
+++ backend/database/SportMember.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import sqlalchemy as sql
+from test import db
+from flask_sqlalchemy import SQLAlchemy
+
+
+class SportMember(db.Model):
+    id = sql.Column(sql.Integer, primary_key=True)
+    member_id = sql.Column(sql.Integer, sql.ForeignKey(
+        'member.id'), nullable=False)
+    sport_id = sql.Column(
+        sql.Integer, sql.ForeignKey('sport.id'), nullable=False)
+    is_trainer = sql.Column(sql.Boolean, nullable=False)
