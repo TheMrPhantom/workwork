cc271a12913e094ac23b211bd3949f0190db825c
schieljn <diemilbe@gmx.net>
2022-01-24 23:29:00 +0100

    disabled SQLALCHEMY_TRACK_MODIFICATIONS

--- backend/web.py
+++ backend/web.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 if os.environ.get("db_name"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("db_name")
 else:
