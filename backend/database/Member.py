01651af36f7c34094ba2265727e88d417e791673
schieljn <diemilbe@gmx.net>
2022-01-24 23:02:12 +0100

    fixed length for salt

--- backend/database/Member.py
+++ backend/database/Member.py
@@ -12,7 +12,7 @@ class Member(db.Model):
     password = sql.Column(sql.LargeBinary(length=128), nullable=False)
     role = sql.Column(sql.Integer, default=0, nullable=False)
     is_deleted = sql.Column(sql.Boolean, default=False, nullable=False)
-    salt = sql.Column(sql.String(32), nullable=False)
+    salt = sql.Column(sql.String(64), nullable=False)
     extra_hours = sql.Column(sql.Integer, default=0, nullable=False)
     last_modified = sql.Column(sql.DateTime, default=datetime.utcnow)
     worktime = relationship('database.Worktime.Worktime', lazy="joined")
