7a54b98ec11811f03cb615d456dc6931fb069bab
schieljn <diemilbe@gmx.net>
2022-01-24 01:29:27 +0100

    progress

--- backend/database/EventParticipant.py
+++ backend/database/EventParticipant.py
@@ -6,5 +6,5 @@ class EventParticipant(db.Model):
     id = sql.Column(sql.Integer, primary_key=True)
     member_id = sql.Column(sql.Integer, sql.ForeignKey(
         'member.id'), nullable=False)
-    sport_id = sql.Column(
+    timeslot_id = sql.Column(
         sql.Integer, sql.ForeignKey('timeslot.id'), nullable=False)
--- backend/database/Timeslot.py
+++ backend/database/Timeslot.py
@@ -4,7 +4,7 @@ from test import db
 
 class Timeslot(db.Model):
     id = sql.Column(sql.Integer, primary_key=True)
-    member_id = sql.Column(sql.Integer, sql.ForeignKey(
+    event_id = sql.Column(sql.Integer, sql.ForeignKey(
         'event.id'), nullable=False)
     name = sql.Column(sql.String(100), nullable=False)
     helper = sql.Column(sql.Integer, nullable=False)
