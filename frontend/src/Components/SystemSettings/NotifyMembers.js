330fede1eb1dee105aedba439c56686f690a20f4
schieljn <diemilbe@gmx.net>
2022-02-01 23:32:57 +0100

    added notify member button

---
+++ frontend/src/Components/SystemSettings/NotifyMembers.js
@@ -0,0 +1,12 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+
+const NotifyMembers = () => {
+    const buttonOnClick = () => {
+        //Todo
+    }
+
+    return <Button variant='outlined' onClick={() => buttonOnClick()}>Mitglieder benachrichtigen</Button>;
+};
+
+export default NotifyMembers;
--- frontend/src/Components/SystemSettings/SystemSettings.js
+++ frontend/src/Components/SystemSettings/SystemSettings.js
@@ -3,6 +3,7 @@ import React from 'react';
 import "../Common/Common.css"
 import Spacer from '../Common/Spacer';
 import HalfYearSetter from './HalfYearSetter';
+import NotifyMembers from './NotifyMembers';
 import SharedWorkTimeSetter from './SharedWorkTimeSetter';
 import YearEndButton from './YearEndButton';
 
@@ -20,6 +21,12 @@ const SystemSettings = () => {
             <Typography variant='caption'>Datum zu dem nur noch die Hälfte geleistet werden muss</Typography>
             <Spacer vertical={20} />
             <HalfYearSetter />
+        </Paper>
+        <Paper className='paperContainer workInProgress'>
+            <Typography variant='h5'>Mitglieder Benachrichtigen</Typography>
+            <Typography variant='caption'>Mitglieder über ihre offenen Arbeitsstunden per Mail informieren</Typography>
+            <Spacer vertical={20} />
+            <NotifyMembers />
         </Paper>
         <Paper className='paperContainer'>
             <Typography variant='h5'>Jahresabschluss</Typography>
