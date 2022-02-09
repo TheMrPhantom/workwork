f179c2525ccf637c60c9d41f2451447bd0996e52
schieljn <diemilbe@gmx.net>
2022-02-09 01:49:37 +0100

    Started dark theme

--- frontend/src/Components/Common/Common.js
+++ frontend/src/Components/Common/Common.js
@@ -3,11 +3,63 @@ import { createTheme } from '@mui/material/styles';
 export const theme = createTheme({
     palette: {
         primary: {
+            main: '#064663'
+        },
+        background: {
+            default: "#041C32"
+        },
+        text: {
+            primary: '#ECB365'
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    color: '#ffffff',
+                },
+                outlined: {
+                    borderColor: "#c8c8c8",
+                    '&:hover': {
+                        borderColor: '#989898'
+                    }
+                }
+            },
+        },
+        MuiDrawer: {
+            styleOverrides: {
+                paper: {
+                    backgroundColor: "#05223d"
+                }
+            }
+        }, MuiListItemIcon: {
+            styleOverrides: {
+                root: {
+                    color: "#ECB365"
+                }
+            }
+        },
+        MuiPaper: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: "#05223d"
+                }
+            }
+        }
+    },
+});
+
+export const themeLight = createTheme({
+    palette: {
+        primary: {
             main: '#bbc34f'
         },
-        buttons: {
-            main: '#000000'
-        }
+        background: {
+            default: "#fbfbfb"
+        },
+        text: {
+            primary: '#000000'
+        },
     },
     components: {
         MuiButton: {
@@ -22,6 +74,6 @@ export const theme = createTheme({
                     }
                 }
             },
-        }
+        },
     },
 });
